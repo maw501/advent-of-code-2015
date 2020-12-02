50d8eae215c0173df9fcbfb8b491a53965f37a13
markworrall <mark.worrall@faculty.ai>
2020-12-02 09:11:59 +0000

    small refactor

--- src/main/java/days/Day2.java
+++ src/main/java/days/Day2.java
@@ -27,6 +27,15 @@ public class Day2 {
         return s.split("x");
     }
 
+    public static int[] convertStringDimsToIntegers(String s) {
+        String[] presentString = splitPresentString(s);
+        int array[] = new int[3];
+        array[0] = Integer.parseInt(presentString[0]);
+        array[1] = Integer.parseInt(presentString[1]);
+        array[2] = Integer.parseInt(presentString[2]);
+        return array;
+    }
+
     public static int calculateAllPresents(ArrayList<String> data) {
         int totalPaper = 0;
         for (String s : data) {
@@ -36,13 +45,10 @@ public class Day2 {
     }
 
     public static int calculateSinglePresent(String s) {
-        String[] presentString = splitPresentString(s);
-        int presentLength = Integer.parseInt(presentString[0]);
-        int presentWidth = Integer.parseInt(presentString[1]);
-        int presentHeight = Integer.parseInt(presentString[2]);
-        int lw = presentLength*presentWidth;
-        int wh = presentWidth*presentHeight;
-        int hl = presentHeight*presentLength;
+        int[] array = convertStringDimsToIntegers(s);
+        int lw = array[0]*array[1];
+        int wh = array[1]*array[2];
+        int hl = array[2]*array[0];
         int smallestSide = Math.min(lw, Math.min(wh, hl));
         int requiredPaper = 2*lw + 2*wh + 2*hl + smallestSide;
         return requiredPaper;

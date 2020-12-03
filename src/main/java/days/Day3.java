671ae1f81465cc018972f01cacf5305192d9c576
markworrall <mark.worrall@faculty.ai>
2020-12-03 20:34:35 +0000

    WIP: day 3 both stars

--- src/main/java/days/Day3.java
+++ src/main/java/days/Day3.java
@@ -2,6 +2,7 @@ package main.java.days;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Day3 {
     static int day = 3;
@@ -13,40 +14,82 @@ public class Day3 {
     }
 
     public static int starOne(ArrayList<String> data) {
+//        String line = data.get(0);
+//        HashMap<String, Integer> hmap = new HashMap<String, Integer>();
+//        hmap.put("0_0", 1);
+//        Integer currX = 0;
+//        Integer currY = 0;
+//        for (int i = 0; i < line.length(); i++) {
+//            char c = line.charAt(i);
+//            currX = updateX(c, currX);
+//            currY = updateY(c, currY);
+//            String currentPositionKey = createHashMapKey(currX, currY);
+//            updateHashMap(hmap, currentPositionKey);
+//        }
+//        return hmap.size();
+        return calculateVisits(data.get(0)).size();
+    }
 
+    public static HashMap<String, Integer> calculateVisits(String line) {
         HashMap<String, Integer> hmap = new HashMap<String, Integer>();
         hmap.put("0_0", 1);
-        String line = data.get(0);
         Integer currX = 0;
         Integer currY = 0;
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
-            if (c == '^') {
-                currY++;
-            } else if (c == '>') {
-                currX++;
-            } else if (c == 'v') {
-                currY--;
-            } else if (c == '<') {
-                currX--;
-            } else {
-                throw new java.lang.RuntimeException("What the fluff");
-            }
+            currX = updateX(c, currX);
+            currY = updateY(c, currY);
             String currentPositionKey = createHashMapKey(currX, currY);
-            if (hmap.containsKey(currentPositionKey)) {
-                hmap.put(currentPositionKey, hmap.get(currentPositionKey) + 1);
-            } else {
-                hmap.put(currentPositionKey, 1);
-            }
+            updateHashMap(hmap, currentPositionKey);
         }
-        return hmap.size();
+        return hmap;
+    }
+
+    public static int starTwo(ArrayList<String> data) {
+        String line = data.get(0);
+        // Santa
+        List<Character> santaTurns = new ArrayList<Character>();
+        for (int i = 0; i < line.length(); i = i + 2) {
+            santaTurns.add(line.charAt(i));
+        }
+        // Robo Santa
+        List<Character> roboSantaTurns = new ArrayList<Character>();
+        for (int i = 1; i < line.length(); i = i + 2) {
+            roboSantaTurns.add(line.charAt(i));
+        }
+        String santaString = new String(String.valueOf(santaTurns));
+        String roboString = new String(String.valueOf(roboSantaTurns));
+
+        HashMap s = calculateVisits(santaString);
+        HashMap rs = calculateVisits(roboString);
+        s.putAll(rs);
+        return s.size();
+    }
+
+    public static void updateHashMap(HashMap hmap, String key) {
+        if (hmap.containsKey(key)) {
+            int i = (int) hmap.get(key);
+            hmap.put(key, i + 1);
+        } else {
+            hmap.put(key, 1);
+        }
+    }
+
+    public static Integer updateX(char c, Integer i) {
+        if (c == '>') return i + 1;
+        if (c == '<') return i - 1;
+        return i;
+    }
+
+    public static Integer updateY(char c, Integer i) {
+        if (c == '^') return i + 1;
+        if (c == 'v') return i - 1;
+        return i;
     }
 
     public static String createHashMapKey(Integer x, Integer y) {
         return Integer.toString(x) + "_" + Integer.toString(y);
     }
 
-    public static int starTwo(ArrayList<String> data) {
-        return 0;
-    }
+
 }

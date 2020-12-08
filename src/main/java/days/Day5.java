5c25f4ab0fda5a8cda6b5001049653eeeae81138
markworrall <mark.worrall@faculty.ai>
2020-12-08 08:18:35 +0000

    day 5 start

---
+++ src/main/java/days/Day5.java
@@ -0,0 +1,21 @@
+package main.java.days;
+
+import java.util.ArrayList;
+
+public class Day5 {
+    static int day = 5;
+
+    public static void main(String[] args) {
+        ArrayList<String> data = main.java.utils.ReadTextFile.readFile(day);
+        System.out.println("Day " + day + " star 1: " + starOne(data));
+        System.out.println("Day " + day + " star 2: " + starTwo(data));
+    }
+
+    public static int starOne(ArrayList<String> data) {
+        return 1;
+    }
+
+    public static int starTwo(ArrayList<String> data) {
+        return 1;
+    }
+}

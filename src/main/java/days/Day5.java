04adb8a140c6fad60240f56897c24d5c6a206581
markworrall <mark.worrall@faculty.ai>
2020-12-08 09:12:57 +0000

    wip day 5

--- src/main/java/days/Day5.java
+++ src/main/java/days/Day5.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Day5 {
     static int day = 5;
+    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
     public static void main(String[] args) {
         ArrayList<String> data = main.java.utils.ReadTextFile.readFile(day);
@@ -12,10 +14,32 @@ public class Day5 {
     }
 
     public static int starOne(ArrayList<String> data) {
+        for (String s : data) {
+            stringHasThreeVowels(s);
+        }
         return 1;
     }
 
     public static int starTwo(ArrayList<String> data) {
         return 1;
     }
+
+    public static boolean stringHasThreeVowels(String s) {
+        int counter = 0;
+        for (char c : vowels) {
+            counter = counter + countCharInString(s, c);
+        }
+        return counter >= 3;
+    }
+
+    public static int countCharInString(String s, char c) {
+        int counter = 0;
+        for (int index = s.indexOf(c);
+             index >= 0;
+             index = s.indexOf(c, index + 1)) {
+            counter++;
+        }
+        return counter;
+    }
+
 }

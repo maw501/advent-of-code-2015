ec3b83c895a3d01da16cb2a418b44de97e4d730f
markworrall <mark.worrall@faculty.ai>
2020-12-09 18:36:40 +0000

    day 5 complete

--- src/main/java/days/Day5.java
+++ src/main/java/days/Day5.java
@@ -1,7 +1,7 @@
 package main.java.days;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Day5 {
     static int day = 5;
@@ -27,38 +27,28 @@ public class Day5 {
     public static int starTwo(ArrayList<String> data) {
         int counter = 0;
         for (String s : data) {
-            boolean c1 = stringHasTwoPairs(s, 0);
-            boolean c2 = stringHasTwoPairs(s, 1);
-            boolean c3 = stringLetterRepeatsByOne(s, 0);
-            boolean c4 = stringLetterRepeatsByOne(s, 1);
-            //System.out.println(s + " : " + c3 + " : " + c4);
-            if ((c1 || c2) && (c3 || c4)) counter++;
-            //System.out.println(s + " even: " + c1 + " odd: " + c2);
+            if (stringHasTwoPairs(s) && stringLetterRepeatsByOne(s)) counter++;
         }
         return counter;
     }
 
-    public static boolean stringHasTwoPairs(String s, int startIndex) {
-        HashSet<String> hash = new HashSet<>();
-        hash.add(s.substring(startIndex, startIndex + 2));
-        int stringLength = s.length();
-        for (int i = startIndex + 2; i < stringLength - 1; i = i + 2) {
+    public static boolean stringHasTwoPairs(String s) {
+        HashMap<String, Integer> hmap = new HashMap<>();
+        for (int i = 0; i < s.length() - 1; i = i + 1) {
             String sub = s.substring(i, i + 2);
-            if (hash.contains(sub)) {
-//                System.out.println(
-//                        s + " found: " + sub + " start: " + startIndex);
-                return true;
+            if (hmap.containsKey(sub)) {
+                int prevIndex = hmap.get(sub);
+                if (i - prevIndex > 1) return true;
+            } else {
+                hmap.put(sub, i);
             }
         }
         return false;
     }
 
-    public static boolean stringLetterRepeatsByOne(String s, int startIndex) {
-        int stringLength = s.length();
-        char prevChar = s.charAt(0);
-        for (int i = startIndex + 2; i < stringLength - 1; i = i + 2) {
-            char currChar = s.charAt(i);
-            if (prevChar == currChar) return true;
+    public static boolean stringLetterRepeatsByOne(String s) {
+        for (int i = 2; i < s.length(); i = i + 1) {
+            if (s.charAt(i) == s.charAt(i - 2)) return true;
         }
         return false;
     }

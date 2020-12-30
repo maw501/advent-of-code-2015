120e6de49438c2328f29e98b632f8de975ec2c01
maw501 <maw501@gmail.com>
2020-12-30 12:58:21 +0000

    refactor into a Grid class

--- src/main/java/days/Day6.java
+++ src/main/java/days/Day6.java
@@ -6,67 +6,31 @@ import java.util.Arrays;
 public class Day6 {
     static int day = 6;
     static int gridDim = 1000;
-    static String onMode = "TURN ON";
-    static String offMode = "TURN OFF";
-    static String toggleMode = "TOGGLE";
-    static int[][] grid = new int[gridDim][gridDim];
 
     public static void main(String[] args) {
         ArrayList<String> data = main.java.utils.ReadTextFile.readFile(day);
-        //System.out.println("Day " + day + " star 1: " + starOne(data));
+        System.out.println("Day " + day + " star 1: " + starOne(data));
         System.out.println("Day " + day + " star 2: " + starTwo(data));
-        //starOne(data);
-        //starTwo(data);
     }
 
     public static int starOne(ArrayList<String> data) {
+        Grid gridStarOne = new Grid(gridDim);
         for (String s : data) {
             String mode = parseModeFromInstruction(s);
             int[] numbers = parseNumbersFromInstruction(s);
-            updateGridFromInstruction(numbers, mode);
+            gridStarOne.updateGridFromInstruction(numbers, mode);
         }
-        return sum2DArray(grid);
+        return gridStarOne.sumGrid();
     }
 
     public static int starTwo(ArrayList<String> data) {
+        Grid gridStarTwo = new Grid(gridDim);
         for (String s : data) {
             String mode = parseModeFromInstruction(s);
             int[] numbers = parseNumbersFromInstruction(s);
-            updateGridFromInstructionBrightness(numbers, mode);
-
+            gridStarTwo.updateGridFromInstructionBrightness(numbers, mode);
         }
-        return sum2DArray(grid);
-    }
-
-
-    public static void updateGridFromInstruction(int[] numbers, String mode) {
-        for (int i = numbers[0]; i <= numbers[2]; i++) {
-            for (int j = numbers[1]; j <= numbers[3]; j++) {
-                updateGrid(i, j, mode);
-            }
-        }
-    }
-
-    public static void updateGridFromInstructionBrightness(int[] numbers, String mode) {
-        for (int i = numbers[0]; i <= numbers[2]; i++) {
-            for (int j = numbers[1]; j <= numbers[3]; j++) {
-                updateGridBrightness(i, j, mode);
-            }
-        }
-    }
-
-    public static void updateGrid(int i, int j, String mode) {
-        int currValue = grid[i][j];
-        if (mode.equals(toggleMode)) grid[i][j] = 1 - currValue;
-        if (mode.equals(onMode)) grid[i][j] = 1;
-        if (mode.equals(offMode)) grid[i][j] = 0;
-    }
-
-    public static void updateGridBrightness(int i, int j, String mode) {
-        int currValue = grid[i][j];
-        if (mode.equals(toggleMode)) grid[i][j] = currValue + 2;
-        if (mode.equals(onMode)) grid[i][j] = currValue + 1;
-        if (mode.equals(offMode)) grid[i][j] = Math.max(0, currValue - 1);
+        return gridStarTwo.sumGrid();
     }
 
     public static int[] parseNumbersFromInstruction(String s) {
@@ -93,18 +57,59 @@ public class Day6 {
 
     public static String parseModeFromInstruction(String s) {
         if (s.startsWith("toggle")) {
-            return toggleMode;
+            return "toggle";
         } else if (s.startsWith("turn on")) {
-            return onMode;
+            return "turn on";
         } else if (s.startsWith("turn off")) {
-            return offMode;
+            return "turn off";
         } else {
             return "Unknown mode";
         }
     }
+}
 
-    public static int sum2DArray(int[][] array) {
-        return Arrays.stream(array)
+class Grid {
+    private int[][] grid;
+    private String onMode = "turn on";
+    private String offMode = "turn off";
+    private String toggleMode = "toggle";
+
+    Grid(int gridDim) {
+        grid = new int[gridDim][gridDim];
+    }
+
+    public void updateGridFromInstruction(int[] numbers, String mode) {
+        for (int i = numbers[0]; i <= numbers[2]; i++) {
+            for (int j = numbers[1]; j <= numbers[3]; j++) {
+                updateGrid(i, j, mode);
+            }
+        }
+    }
+
+    public void updateGridFromInstructionBrightness(int[] numbers, String mode) {
+        for (int i = numbers[0]; i <= numbers[2]; i++) {
+            for (int j = numbers[1]; j <= numbers[3]; j++) {
+                updateGridBrightness(i, j, mode);
+            }
+        }
+    }
+
+    public void updateGrid(int i, int j, String mode) {
+        int currValue = grid[i][j];
+        if (mode.equals(toggleMode)) grid[i][j] = 1 - currValue;
+        if (mode.equals(onMode)) grid[i][j] = 1;
+        if (mode.equals(offMode)) grid[i][j] = 0;
+    }
+
+    public void updateGridBrightness(int i, int j, String mode) {
+        int currValue = grid[i][j];
+        if (mode.equals(toggleMode)) grid[i][j] = currValue + 2;
+        if (mode.equals(onMode)) grid[i][j] = currValue + 1;
+        if (mode.equals(offMode)) grid[i][j] = Math.max(0, currValue - 1);
+    }
+
+    public int sumGrid() {
+        return Arrays.stream(grid)
                 .flatMapToInt(Arrays::stream)
                 .sum();
     }

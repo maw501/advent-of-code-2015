7649239b1673ec914908d7419f7b8e37a23ff139
maw501 <maw501@gmail.com>
2020-12-30 12:26:53 +0000

    day 6 part 2 complete

--- src/main/java/days/Day6.java
+++ src/main/java/days/Day6.java
@@ -6,13 +6,17 @@ import java.util.Arrays;
 public class Day6 {
     static int day = 6;
     static int gridDim = 1000;
+    static String onMode = "TURN ON";
+    static String offMode = "TURN OFF";
+    static String toggleMode = "TOGGLE";
     static int[][] grid = new int[gridDim][gridDim];
 
     public static void main(String[] args) {
         ArrayList<String> data = main.java.utils.ReadTextFile.readFile(day);
-        System.out.println("Day " + day + " star 1: " + starOne(data));
+        //System.out.println("Day " + day + " star 1: " + starOne(data));
         System.out.println("Day " + day + " star 2: " + starTwo(data));
-        starOne(data);
+        //starOne(data);
+        //starTwo(data);
     }
 
     public static int starOne(ArrayList<String> data) {
@@ -25,7 +29,13 @@ public class Day6 {
     }
 
     public static int starTwo(ArrayList<String> data) {
-        return 1;
+        for (String s : data) {
+            String mode = parseModeFromInstruction(s);
+            int[] numbers = parseNumbersFromInstruction(s);
+            updateGridFromInstructionBrightness(numbers, mode);
+
+        }
+        return sum2DArray(grid);
     }
 
 
@@ -37,13 +47,27 @@ public class Day6 {
         }
     }
 
-    public static void updateGrid(int i, int j, String mode) {
-        int currValue = grid[i][j];
-        if (mode.equals("TOGGLE")) grid[i][j] = 1 - currValue;
-        if (mode.equals("TURN ON")) grid[i][j] = 1;
-        if (mode.equals("TURN OFF")) grid[i][j] = 0;
+    public static void updateGridFromInstructionBrightness(int[] numbers, String mode) {
+        for (int i = numbers[0]; i <= numbers[2]; i++) {
+            for (int j = numbers[1]; j <= numbers[3]; j++) {
+                updateGridBrightness(i, j, mode);
+            }
+        }
     }
 
+    public static void updateGrid(int i, int j, String mode) {
+        int currValue = grid[i][j];
+        if (mode.equals(toggleMode)) grid[i][j] = 1 - currValue;
+        if (mode.equals(onMode)) grid[i][j] = 1;
+        if (mode.equals(offMode)) grid[i][j] = 0;
+    }
+
+    public static void updateGridBrightness(int i, int j, String mode) {
+        int currValue = grid[i][j];
+        if (mode.equals(toggleMode)) grid[i][j] = currValue + 2;
+        if (mode.equals(onMode)) grid[i][j] = currValue + 1;
+        if (mode.equals(offMode)) grid[i][j] = Math.max(0, currValue - 1);
+    }
 
     public static int[] parseNumbersFromInstruction(String s) {
         String out = "";
@@ -69,11 +93,11 @@ public class Day6 {
 
     public static String parseModeFromInstruction(String s) {
         if (s.startsWith("toggle")) {
-            return "TOGGLE";
+            return toggleMode;
         } else if (s.startsWith("turn on")) {
-            return "TURN ON";
+            return onMode;
         } else if (s.startsWith("turn off")) {
-            return "TURN OFF";
+            return offMode;
         } else {
             return "Unknown mode";
         }

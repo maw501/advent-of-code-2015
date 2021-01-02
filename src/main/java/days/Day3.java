f2920c968ef5180e904994b091950980dc7a34c7
maw501 <maw501@gmail.com>
2021-01-02 18:39:21 +0000

    tidy day 3 a little

--- src/main/java/days/Day3.java
+++ src/main/java/days/Day3.java
@@ -5,31 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Day3 {
-  static int day = 3;
+  private static final int day = 3;
 
   public static void main(String[] args) {
-    ArrayList<String> data = main.java.utils.ReadTextFile.readFile(day);
-    System.out.println("Day " + day + " star 1: " + starOne(data));
-    System.out.println("Day " + day + " star 2: " + starTwo(data));
+    var data = main.java.utils.ReadTextFile.readFile(day);
+    Day3 day3 = new Day3();
+    System.out.println("Day " + day + " star 1: " + day3.starOne(data));
+    System.out.println("Day " + day + " star 2: " + day3.starTwo(data));
   }
 
-  public static int starOne(ArrayList<String> data) {
+  private int starOne(ArrayList<String> data) {
     return calculateVisits(data.get(0)).size();
   }
 
-  public static int starTwo(ArrayList<String> data) {
+  private int starTwo(ArrayList<String> data) {
     String line = data.get(0);
 
-    List santaTurns = parseSantaTurns(line, 0);
-    List roboSantaTurns = parseSantaTurns(line, 1);
+    List<Character> santaTurns = parseSantaTurns(line, 0);
+    List<Character> roboSantaTurns = parseSantaTurns(line, 1);
 
-    HashMap s = calculateVisits(String.valueOf(santaTurns));
-    HashMap rs = calculateVisits(String.valueOf(roboSantaTurns));
+    HashMap<String, Integer> s = calculateVisits(String.valueOf(santaTurns));
+    HashMap<String, Integer> rs = calculateVisits(String.valueOf(roboSantaTurns));
     s.putAll(rs);
     return s.size();
   }
 
-  public static List parseSantaTurns(String line, int startIndex) {
+  private List<Character> parseSantaTurns(String line, int startIndex) {
     List<Character> santaTurns = new ArrayList<>();
     for (int i = startIndex; i < line.length(); i = i + 2) {
       santaTurns.add(line.charAt(i));
@@ -37,43 +38,42 @@ public class Day3 {
     return santaTurns;
   }
 
-  public static HashMap<String, Integer> calculateVisits(String line) {
+  private HashMap<String, Integer> calculateVisits(String line) {
     HashMap<String, Integer> hmap = new HashMap<>();
     hmap.put("0_0", 1);
     Integer currX = 0;
     Integer currY = 0;
     for (int i = 0; i < line.length(); i++) {
-      char c = line.charAt(i);
-      currX = updateX(c, currX);
-      currY = updateY(c, currY);
+      currX = updateX(line.charAt(i), currX);
+      currY = updateY(line.charAt(i), currY);
       String currentPositionKey = createHashMapKey(currX, currY);
       updateHashMap(hmap, currentPositionKey);
     }
     return hmap;
   }
 
-  public static void updateHashMap(HashMap hmap, String key) {
+  private void updateHashMap(HashMap<String, Integer> hmap, String key) {
     if (hmap.containsKey(key)) {
-      int i = (int) hmap.get(key);
-      hmap.put(key, i + 1);
+      int i = hmap.get(key);
+      hmap.put(key, --i);
     } else {
       hmap.put(key, 1);
     }
   }
 
-  public static Integer updateX(char c, Integer i) {
-    if (c == '>') return i + 1;
-    if (c == '<') return i - 1;
+  private Integer updateX(char c, Integer i) {
+    if (c == '>') return ++i;
+    if (c == '<') return --i;
     return i;
   }
 
-  public static Integer updateY(char c, Integer i) {
-    if (c == '^') return i + 1;
-    if (c == 'v') return i - 1;
+  private Integer updateY(char c, Integer i) {
+    if (c == '^') return ++i;
+    if (c == 'v') return --i;
     return i;
   }
 
-  public static String createHashMapKey(Integer x, Integer y) {
+  private String createHashMapKey(Integer x, Integer y) {
     return x + "_" + y;
   }
 }

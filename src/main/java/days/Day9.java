f8c82f48d856fa5fd39f8ef5578ff293dd3951d8
maw501 <maw501@gmail.com>
2021-01-04 18:54:33 +0000

    Day 9 complete

--- src/main/java/days/Day9.java
+++ src/main/java/days/Day9.java
@@ -1,24 +1,49 @@
 package main.java.days;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
-public class Day9<out> {
+public class Day9 {
   private static final int day = 9;
-  HashMap<String, Integer> distances = new HashMap<>();
+  HashMap<String, Integer> cityDistances = new HashMap<>();
+  ArrayList<Integer> distances = new ArrayList<>();
+  Set<String> allCities = new HashSet<>();
 
   public static void main(String[] args) {
     ArrayList<String> data = main.java.utils.ReadTextFile.readFile(day);
     Day9 day9 = new Day9();
-    day9.starOne(data);
-    //System.out.println("Day " + day + " star 1: " + day9.starOne(data));
+    day9.buildDistanceMap(data);
+    day9.calculateDistances();
+    System.out.println("Day " + day + " star 1: " + day9.starOne());
+    System.out.println("Day " + day + " star 2: " + day9.starTwo());
   }
 
-  private int starOne(ArrayList<String> data) {
-    buildDistanceMap(data);
-    buildPermutations();
-    return 1;
+  private int starOne() {
+    return Collections.min(distances);
+  }
+
+  private int starTwo() {
+    return Collections.max(distances);
+  }
+
+  private void calculateDistances() {
+    ArrayList<ArrayList<String>> perms = buildPermutations();
+    for (ArrayList<String> journey : perms) {
+      int distance = calculateJourneyDistance(journey);
+      distances.add(distance);
+    }
+  }
+
+  private int calculateJourneyDistance(ArrayList<String> journey) {
+    String prevLocation = journey.get(0);
+    int totalJourneyDistance = 0;
+    for (int i = 1; i < journey.size(); i++) {
+      String currLocation = journey.get(i);
+      String key = prevLocation + "_" + currLocation;
+      int distance = cityDistances.get(key);
+      totalJourneyDistance = totalJourneyDistance + distance;
+      prevLocation = currLocation;
+    }
+    return totalJourneyDistance;
   }
 
   private void buildDistanceMap(ArrayList<String> data) {
@@ -26,25 +51,47 @@ public class Day9<out> {
       String[] result = s.split(" ");
       String key1 = result[0] + "_" + result[2];
       String key2 = result[2] + "_" + result[0];
-      distances.put(key1, Integer.parseInt(result[4]));
-      distances.put(key2, Integer.parseInt(result[4]));
+      cityDistances.put(key1, Integer.parseInt(result[4]));
+      cityDistances.put(key2, Integer.parseInt(result[4]));
+      allCities.add(result[0]);
+      allCities.add(result[2]);
     }
   }
 
-  private void buildPermutations() {
-    ArrayList<String> arr = new ArrayList<>();
-    arr.add("a");
-    arr.add("b");
-    arr.add("c");
-    int n = 3;
-    ArrayList<String[]> outArray = new ArrayList<>();
-    String [] e1 = {arr.get(0), arr.get(1)};
-    String [] e2 = {arr.get(1), arr.get(0)};
-    outArray.add(e1);
-    outArray.add(e2);
-    for (String[] s : outArray){
-      System.out.println(Arrays.toString(s));
+  private ArrayList<ArrayList<String>> buildPermutations() {
+    ArrayList<String> cities = new ArrayList<>(allCities);
+    ArrayList<ArrayList<String>> currArray = new ArrayList<>();
+    ArrayList<String> e1 = new ArrayList<>();
+    e1.add(cities.get(0));
+    currArray.add(e1);
+
+    int currIndex = 1;
+    int currLen = 0;
+    long finalLength = factorial(cities.size());
+    ArrayList<ArrayList<String>> finalArray = new ArrayList<>();
+
+    while (currLen < finalLength) {
+      finalArray = new ArrayList<>();
+      String currString = cities.get(currIndex);
+      for (ArrayList<String> element : currArray) {
+        for (int i = 0; i <= currIndex; i++) {
+          ArrayList<String> newElement = new ArrayList<>(element);
+          newElement.add(i, currString);
+          finalArray.add(newElement);
+        }
+      }
+      currLen = finalArray.size();
+      currIndex++;
+      currArray = new ArrayList<>(finalArray);
     }
-    int currIndex = 2;
+    return finalArray;
+  }
+
+  private long factorial(long x) {
+    long result = 1;
+    for (long i = x; i > 0; i--) {
+      result = result * i;
+    }
+    return result;
   }
 }

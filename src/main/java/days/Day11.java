185d6f1419da0b3b70d9da7aa4f9f21f98848c2e
maw501 <maw501@gmail.com>
2021-01-09 16:56:31 +0000

    Day 11 complete

--- src/main/java/days/Day11.java
+++ src/main/java/days/Day11.java
@@ -1,35 +1,33 @@
 package main.java.days;
 
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day11 {
   private static final int day = 11;
   private String bannedChars = "iol";
-  private Pattern regex = Pattern.compile("([a-z])\1");
+  private String regex = "(?<=(.))(?!\\1)";
 
   public static void main(String[] args) {
-    String start = "xx";
+    String starOneInput = "vzbxkghb";
     Day11 day11 = new Day11();
-    System.out.println("Day " + day + " star 1: " + day11.starOne());
-    System.out.println("Day " + day + " star 1: " + day11.starTwo());
+    String starOneAnswer = day11.starOne(starOneInput);
+    System.out.println("Day " + day + " star 1: " + starOneAnswer);
+    System.out.println("Day " + day + " star 2: " + day11.starOne(starOneAnswer));
   }
 
-  private int starOne() {
-    String start = "yyazz";
-    String incrementedString = incrementString(start);
-    boolean c1 = hasNoBannedChars(incrementedString);
-    Matcher matcher = regex.matcher(incrementedString);
-    int count = (int) matcher.results().count();
-    System.out.println(count);
-
-    return 1;
-  }
-
-  private int starTwo() {
-    return 1;
+  private String starOne(String start) {
+    String incrementedString = start;
+    boolean validPassword = false;
+    while (!validPassword) {
+      incrementedString = incrementString(start);
+      boolean c1 = hasNoBannedChars(incrementedString);
+      boolean c2 = hasDoubleChars(incrementedString);
+      boolean c3 = hasIncreasingChars(incrementedString);
+      validPassword = c1 && c2 && c3;
+      start = incrementedString;
+    }
+    return incrementedString;
   }
 
   private String incrementString(String s) {
@@ -43,13 +41,28 @@ public class Day11 {
         break;
       }
     }
-    System.out.println("Input string: " + s + " incremented: " + startString);
     return String.valueOf(startString);
   }
 
   private char incrementSingleChar(char c) {
     if (c == 'z') return 'a';
     return (char) (c + 1);
+  }
+
+  private boolean hasIncreasingChars(String s) {
+    int count = 1;
+    int prevChar = s.charAt(0);
+    for (int i = 1; i < s.length(); i++) {
+      int currChar = s.charAt(i);
+      if (currChar - prevChar == 1) {
+        count++;
+      } else {
+        count = 1;
+      }
+      if (count == 3) return true;
+      prevChar = currChar;
+    }
+    return false;
   }
 
   private boolean hasNoBannedChars(String s) {
@@ -59,5 +72,16 @@ public class Day11 {
       }
     }
     return true;
+  }
+
+  private boolean hasDoubleChars(String s) {
+    Set<String> doubleChars = new HashSet<>();
+    String[] matches = s.split(regex);
+    for (String match : matches) {
+      if (match.length() == 2) {
+        doubleChars.add(match);
+      }
+    }
+    return doubleChars.size() >= 2;
   }
 }

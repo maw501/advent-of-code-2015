0c6beb3d21f952a92966c8b864ecdeee8d84d728
markworrall <mark.worrall@faculty.ai>
2021-01-12 11:48:29 +0000

    Day 12 complete - needs refactor

--- src/main/java/days/Day12.java
+++ src/main/java/days/Day12.java
@@ -4,40 +4,57 @@ import com.github.cliftonlabs.json_simple.*;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Set;
 
 public class Day12 {
-  private static final int day = 12;
-  private long currentTotal = 0;
-
   public static void main(String[] args) throws IOException, JsonException {
-    try (FileReader fileReader = new FileReader(("src/main/resources" + "/day12_test" + ".json"))) {
+    try (FileReader fileReader = new FileReader(("src/main/resources" + "/day12" + ".json"))) {
 
       JsonArray deserialize = (JsonArray) Jsoner.deserialize(fileReader);
       System.out.println(deserialize);
       System.out.println("--------------------------");
       Day12 day12 = new Day12();
-      day12.iterateOverJsonArray(deserialize);
+      long answer = day12.iterateOverJsonArray(deserialize);
+      System.out.println(answer);
     }
   }
 
-  private void iterateOverJsonArray(JsonArray array) {
-    for (Object j : array) {
-      if (j instanceof JsonObject) {
-        iterateOverJsonObject((JsonObject) j);
-      } else if (j instanceof JsonArray) {
-        JsonArray k = (JsonArray) j;
-        System.out.println(k);
+  private long iterateOverJsonArray(JsonArray array) {
+    long arrayValue = 0;
+    for (Object obj : array) {
+      if (obj instanceof JsonObject) {
+        arrayValue += iterateOverJsonObject((JsonObject) obj);
+      } else if (obj instanceof JsonArray) {
+        arrayValue += iterateOverJsonArray((JsonArray) obj);
+      } else if (obj instanceof String) {
+        continue;
       } else {
-        System.out.println(j);
+        arrayValue += ((BigDecimal) obj).longValue();
       }
     }
+    return arrayValue;
   }
 
-  private void iterateOverJsonObject(JsonObject array) {
-    Set keys = array.keySet();
-    for (Object key : keys) {
-      System.out.println("key: " + key + " value: " + array.get(key));
+  private long iterateOverJsonObject(JsonObject object) {
+    long objectValue = 0;
+    Set<String> keys = object.keySet();
+    for (String key : keys) {
+      if (object.get(key) instanceof JsonArray) {
+        objectValue += iterateOverJsonArray((JsonArray) object.get(key));
+      } else if (object.get(key) instanceof JsonObject) {
+        objectValue += iterateOverJsonObject((JsonObject) object.get(key));
+      } else if (object.get(key) instanceof String) {
+        String stringValue = (String) object.get(key);
+        if (stringValue.equals("red")){
+          return 0;
+        }
+        //continue;
+      } else {
+        objectValue += ((BigDecimal) object.get(key)).longValue();
+      }
     }
+    return objectValue;
   }
 }

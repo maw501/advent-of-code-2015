55c6615635d0647388232ffe23b187ab43fbb935
markworrall <mark.worrall@faculty.ai>
2021-01-12 09:01:55 +0000

    Start day 12 very quickly

--- src/main/java/days/Day12.java
+++ src/main/java/days/Day12.java
@@ -4,15 +4,40 @@ import com.github.cliftonlabs.json_simple.*;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Set;
 
 public class Day12 {
   private static final int day = 12;
+  private long currentTotal = 0;
 
   public static void main(String[] args) throws IOException, JsonException {
-    try (FileReader fileReader = new FileReader(("src/main/resources/day12.json"))) {
+    try (FileReader fileReader = new FileReader(("src/main/resources" + "/day12_test" + ".json"))) {
 
       JsonArray deserialize = (JsonArray) Jsoner.deserialize(fileReader);
       System.out.println(deserialize);
+      System.out.println("--------------------------");
+      Day12 day12 = new Day12();
+      day12.iterateOverJsonArray(deserialize);
+    }
+  }
+
+  private void iterateOverJsonArray(JsonArray array) {
+    for (Object j : array) {
+      if (j instanceof JsonObject) {
+        iterateOverJsonObject((JsonObject) j);
+      } else if (j instanceof JsonArray) {
+        JsonArray k = (JsonArray) j;
+        System.out.println(k);
+      } else {
+        System.out.println(j);
+      }
+    }
+  }
+
+  private void iterateOverJsonObject(JsonObject array) {
+    Set keys = array.keySet();
+    for (Object key : keys) {
+      System.out.println("key: " + key + " value: " + array.get(key));
     }
   }
 }

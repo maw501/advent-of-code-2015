ddc42ff2e72c232853af155a9978594cfd0801fd
markworrall <mark.worrall@faculty.ai>
2021-01-12 14:25:44 +0000

    keep only one solution

--- src/main/java/days/Day12.java
+++ src/main/java/days/Day12.java
@@ -13,45 +13,12 @@ public class Day12 {
   public static void main(String[] args) throws IOException, JsonException {
     try (FileReader fileReader = new FileReader(("src/main/resources" + "/day12" + ".json"))) {
       JsonArray deserialize = (JsonArray) Jsoner.deserialize(fileReader);
-      System.out.println(deserialize);
       Day12 day12 = new Day12();
-      long starOneAnswer = day12.iterateOverJsonArray(deserialize, false);
-      long starTwoAnswer = day12.iterateOverJsonArray(deserialize, true);
-      System.out.println("Day " + day + " star 1: " + starOneAnswer);
-      System.out.println("Day " + day + " star 2: " + starTwoAnswer);
-      // Using generic Object
       long starOneAnswerObj = day12.calculateJsonValue(deserialize, false);
       long starTwoAnswerObj = day12.calculateJsonValue(deserialize, true);
-      System.out.println("Day " + day + " star 1 with generics: " + starOneAnswerObj);
-      System.out.println("Day " + day + " star 2 with generics: " + starTwoAnswerObj);
+      System.out.println("Day " + day + " star 1: " + starOneAnswerObj);
+      System.out.println("Day " + day + " star 2: " + starTwoAnswerObj);
     }
-  }
-
-  private long iterateOverJsonArray(JsonArray array, boolean ignoreRed) {
-    long arrayValue = 0;
-    for (Object obj : array) {
-      if (obj instanceof JsonObject)
-        arrayValue += iterateOverJsonObject((JsonObject) obj, ignoreRed);
-      if (obj instanceof JsonArray) arrayValue += iterateOverJsonArray((JsonArray) obj, ignoreRed);
-      if (obj instanceof BigDecimal) arrayValue += ((BigDecimal) obj).longValue();
-    }
-    return arrayValue;
-  }
-
-  private long iterateOverJsonObject(JsonObject object, boolean ignoreRed) {
-    long objectValue = 0;
-    Set<String> keys = object.keySet();
-    for (String key : keys) {
-      if (object.get(key) instanceof JsonArray)
-        objectValue += iterateOverJsonArray((JsonArray) object.get(key), ignoreRed);
-      if (object.get(key) instanceof JsonObject)
-        objectValue += iterateOverJsonObject((JsonObject) object.get(key), ignoreRed);
-      if (object.get(key) instanceof String)
-        if (object.get(key).equals("red") && ignoreRed) return 0;
-      if (object.get(key) instanceof BigDecimal)
-        objectValue += ((BigDecimal) object.get(key)).longValue();
-    }
-    return objectValue;
   }
 
   private long calculateJsonValue(Object obj, boolean ignoreRed) {
@@ -67,7 +34,6 @@ public class Day12 {
         if (value.equals("red") && ignoreRed) return 0;
         objectValue += calculateJsonValue(value, ignoreRed);
       }
-    } else if (obj instanceof String) {
     } else if (obj instanceof BigDecimal) {
       objectValue += ((BigDecimal) obj).longValue();
     }

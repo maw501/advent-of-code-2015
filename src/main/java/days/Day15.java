7669923d15fff49711ed8453419e4d060afdeae5
markworrall <mark.worrall@faculty.ai>
2021-01-22 15:42:50 +0000

    Move calcs to methods - still don't read inputs from file

--- src/main/java/days/Day15.java
+++ src/main/java/days/Day15.java
@@ -1,24 +1,46 @@
 package main.java.days;
 
 public class Day15 {
+  private static final int day = 15;
+
   public static void main(String[] args) {
+    Day15 day15 = new Day15();
+    System.out.println("Day " + day + " star 1: " + day15.calculateBestScore(false));
+    System.out.println("Day " + day + " star 1: " + day15.calculateBestScore(true));
+  }
+
+  private long calculateBestScore(boolean calorieRestriction) {
     long bestScore = 0;
     for (int i = 1; i <= 100; i++) {
       for (int j = 1; j <= 100 - i; j++) {
         for (int k = 1; k <= 100 - i - j; k++) {
           int l = 100 - i - j - k;
-          int capacity = Math.max(0, i * 3 + j * -3 + k * -1);
-          int durability = Math.max(0, 3 * j);
-          int flavour = Math.max(0, 4 * k - 2 * l);
-          int texture = Math.max(0, -3 * i + 2 * l);
-          int calories = i * 2 + j * 9 + k + l * 8;
-          long score = (long) capacity * durability * flavour * texture;
-          if (score > bestScore && calories == 500) {
+          long score = calculateScore(i, j, k, l);
+          if (calorieRestriction) {
+            int calories = calculateCalories(i, j, k, l);
+            if (score > bestScore && calories == 500) {
+              bestScore = score;
+            }
+          } else if (score > bestScore) {
             bestScore = score;
           }
         }
       }
     }
-    System.out.println(bestScore);
+    return bestScore;
+  }
+
+  private int calculateCalories(
+      int weightSugar, int weightSprinkles, int weightCandy, int weightChocolate) {
+    return weightSugar * 2 + weightSprinkles * 9 + weightCandy + weightChocolate * 8;
+  }
+
+  private long calculateScore(
+      int weightSugar, int weightSprinkles, int weightCandy, int weightChocolate) {
+    int capacity = Math.max(0, weightSugar * 3 + weightSprinkles * -3 + weightCandy * -1);
+    int durability = Math.max(0, 3 * weightSprinkles);
+    int flavour = Math.max(0, 4 * weightCandy - 2 * weightChocolate);
+    int texture = Math.max(0, -3 * weightSugar + 2 * weightChocolate);
+    return (long) capacity * durability * flavour * texture;
   }
 }

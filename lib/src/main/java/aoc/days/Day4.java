c429863b700986966ef38a0fa964c80053e7bda1
maw501 <maw501@gmail.com>
2021-04-05 16:30:28 +0100

    Optimize day 4

--- lib/src/main/java/aoc/days/Day4.java
+++ lib/src/main/java/aoc/days/Day4.java
@@ -1,6 +1,5 @@
 package aoc.days;
 
-import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.MessageDigest;
 
@@ -9,52 +8,36 @@ public class Day4 {
   private static final String data = "iwrupvqb";
 
   public static void main(String[] args) {
-    System.out.println("Day " + day + " star 1: " + starOne());
-    System.out.println("Day " + day + " star 2: " + starTwo());
-  }
-
-  private static int starOne() {
-    return findMD5IntegerSuffix(5);
-  }
-
-  private static int starTwo() {
-    return findMD5IntegerSuffix(6);
-  }
-
-  private static int findMD5IntegerSuffix(int length) {
-    int i = 0;
-    while (true) {
-      String hash = getMD5Hash(Day4.data + i);
-      String sub = hash.substring(0, length);
-      if (subStringToInt(sub, length) == 0) {
-        return i;
-      }
-      i++;
-    }
-  }
-
-  private static int subStringToInt(String inputString, int stringLength) {
-    String sub = inputString.substring(0, stringLength);
     try {
-      return Integer.parseInt(sub);
-    } catch (NumberFormatException e) {
-      return 99;
-    }
-  }
-
-  private static String getMD5Hash(String hashInput) {
-    try {
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      byte[] messageDigest = md.digest(hashInput.getBytes());
-      BigInteger no = new BigInteger(1, messageDigest);
-
-      StringBuilder hashtext = new StringBuilder(no.toString(16));
-      while (hashtext.length() < 32) {
-        hashtext.insert(0, "0");
-      }
-      return hashtext.toString();
+      System.out.println("Day " + day + " star 1: " + starOne());
+      System.out.println("Day " + day + " star 2: " + starTwo());
     } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
+      System.out.println("Error");
     }
+  }
+
+  private static int starOne() throws NoSuchAlgorithmException {
+    return findMD5IntegerSuffix(false);
+  }
+
+  private static int starTwo() throws NoSuchAlgorithmException {
+    return findMD5IntegerSuffix(true);
+  }
+
+  private static int findMD5IntegerSuffix(boolean starTwo) throws NoSuchAlgorithmException {
+    int i = 0;
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    byte[] array;
+    while (true) {
+      array = md.digest(data.concat(Integer.toString(i++)).getBytes());
+      if (array[0] == 0 && array[1] == 0 && (array[2] >> 4 & 0xf) == 0) {
+        if (starTwo) {
+          if (array[2] == 0) break;
+        } else {
+          break;
+        }
+      }
+    }
+    return i - 1;
   }
 }
